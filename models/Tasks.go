12fff1f8453e76ba5d34dc748c180a1d76f66131
YtaloWill <ytalowillgloria@gmail.com>
2020-08-21 02:01:46 -0300

    remove required fields in model json annotations

--- models/Tasks.go
+++ models/Tasks.go
@@ -1,13 +1,13 @@
 package models
 
 type Status struct {
-	Id				int 	`json:"!id"`
-	Description		string 	`json:"!description"`
+	Id				int 	`json:"id"`
+	Description		string 	`json:"description"`
 }
 
 type Task struct {
-	Id				int 	`json:"!id"`
-	Title			string 	`json:"!title"`
+	Id				int 	`json:"id"`
+	Title			string 	`json:"title"`
 	Description		string 	`json:"description"`
 	Status			Status 	`json:"status"`
 	IdUser			int 	`json:"iduser"`
--- models/User.go
+++ models/User.go
@@ -1,7 +1,7 @@
 package models
 
 type User struct {
-	Id				int `json:"!id"`
+	Id				int `json:"id"`
 	Name			string `json:"name"`
 	Email			string `json:"email"`
 	Tasks			[]Task `json:"tasks"`
